fc8d0e22ca31e3c3b33ef0eb46dd785ce356aa32
Kaden Johnson <nokadensly@gmail.com>
2022-03-22 10:32:25 -0600

    characters page needs refactor

--- Portfolio/characters/characters.js
+++ Portfolio/characters/characters.js
@@ -6,13 +6,28 @@ const main = document.querySelector('main')
 
 const allCharsButton = document.createElement('button')
 allCharsButton.textContent = 'All Characters'
-allCharsButton.addEventListener('click', function (event) {
-    
+allCharsButton.addEventListener('click', function () {
+    console.log('thanks for clicking',)
   populateDOM()
 })
 header.appendChild(allCharsButton)
 
 function populateDOM() {
-    console.log(people)
+
+    people.forEach((person) => {
+      
+    const personFig = document.createElement('figure')
+    const personImg = document.createElement('img')
+    personImg.src = `https://starwars-visualguide.com/assets/img/characters/1.jpg`
+    const personCap = document.createElement('figcaption')
+    personCap.textContent = person.name
+
+    personFig.appendChild(personImg)
+    personFig.appendChild(personCap)
+    main.appendChild(personFig)})
 }
 
+function getLastNumber(url) {
+  const secondToLastLetterOfUrl = url[url.length -2]
+  retrun secondToLastLetterOfUrl
+}
