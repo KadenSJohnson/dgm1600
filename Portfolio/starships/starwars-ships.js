a18352b5e5ba20fa104aa9c566088981efbb3aab
Kaden Johnson <nokadensly@gmail.com>
2022-04-01 17:00:31 -0600

    added modal for 404 ships

--- Portfolio/starships/starwars-ships.js
+++ Portfolio/starships/starwars-ships.js
@@ -1,26 +1,52 @@
 import { starships } from "../data/starships.js"
+import { removeChildren, getLastNumber } from "../utils/index.js"
 
 
 const nav = document.querySelector('.nav')
 const navlist = document.querySelector('.navlist')
 const shipview = document.querySelector('.shipview')
 
+const modal = document.querySelector('.modal')
+const closeButton = document.querySelector('.modal-close')
+const closeBackGround = document.querySelector('.modal-background')
+const shipMessage = document.querySelector('.shipMessage')
+
+closeButton.addEventListener('click', () => {
+    modal.classList.toggle('is-active')
+})
+
+closeBackGround.addEventListener('click', () => {
+    modal.classList.toggle('is-active')
+})
+
 function populateNav() {
     starships.forEach((starship) => {
-        let anchorWrap = document.createElement('a')
-        anchorWrap.href = '#'
+        let anchor = document.createElement('a')
+        anchor.href = '#'
+        anchor.textContent = starship.name
         const listItem = document.createElement('li')
-        listItem.textContent = starship.name
+        
 
 
-        anchorWrap.addEventListener('click', () => populateShipView(starship))
-        anchorWrap.appendChild(listItem)
-        navlist.appendChild(anchorWrap)
+        anchor.addEventListener('click', () => populateShipView(starship))
+        anchor.appendChild(listItem)
+        navlist.appendChild(anchor)
     })
 }
 
 populateNav()
 
-function populateShipView() {
-    console.log('thanks for clicking', shipData.name)
+function populateShipView(shipData) {
+    removeChildren(shipview) 
+    const shipImg = document.createElement('img')
+    let shipNum = getLastNumber(shipData.url)
+    shipImg.src = `https://starwars-visualguide.com/assets/img/starships/${shipNum}.jpg`
+    
+    shipImg.addEventListener('error', () => {
+        shipImg.hidden = true
+        modal.classList.toggle('is-active')
+        shipMessage.textContent = `${shipData.name} isn't here at the moment `
+    })
+
+    shipview.appendChild(shipImg)
 }
