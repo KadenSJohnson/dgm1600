7f92d31bea43124464f88d348c1c8c8f8b877d97
Kaden Johnson <nokadensly@gmail.com>
2022-04-01 10:07:14 -0600

    added list items with anchors

--- Portfolio/starships/starwars-ships.js
+++ Portfolio/starships/starwars-ships.js
@@ -1,15 +1,26 @@
-import { starships } from "../data/starships";
+import { starships } from "../data/starships.js"
 
 
 const nav = document.querySelector('.nav')
-const navlist = document.querySelector('navlist')
-const shipview = document.querySelector('shipview')
+const navlist = document.querySelector('.navlist')
+const shipview = document.querySelector('.shipview')
 
 function populateNav() {
+    starships.forEach((starship) => {
+        let anchorWrap = document.createElement('a')
+        anchorWrap.href = '#'
+        const listItem = document.createElement('li')
+        listItem.textContent = starship.name
 
+
+        anchorWrap.addEventListener('click', () => populateShipView(starship))
+        anchorWrap.appendChild(listItem)
+        navlist.appendChild(anchorWrap)
+    })
 }
 
+populateNav()
 
 function populateShipView() {
-    
+    console.log('thanks for clicking', shipData.name)
 }
