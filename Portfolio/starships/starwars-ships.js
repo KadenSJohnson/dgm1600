c6aa1ecf76f00e5fa95357f582d8c3e0def035c7
Kaden Johnson <nokadensly@gmail.com>
2022-04-01 09:41:52 -0600

    added starships page

---
+++ Portfolio/starships/starwars-ships.js
@@ -0,0 +1,15 @@
+import { starships } from "../data/starships";
+
+
+const nav = document.querySelector('.nav')
+const navlist = document.querySelector('navlist')
+const shipview = document.querySelector('shipview')
+
+function populateNav() {
+
+}
+
+
+function populateShipView() {
+    
+}
