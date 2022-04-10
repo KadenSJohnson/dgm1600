712a57c332de1791fe3d6e8c9d9e89c1471a3825
Kaden Johnson <nokadensly@gmail.com>
2022-04-09 20:44:41 -0600

    added pokemon page and function to get api data

---
+++ Portfolio/pokemon/pokecode.js
@@ -0,0 +1,15 @@
+function getAPIData(url) {
+    try {
+        return fetch(url).then((data) => data.json())
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+function loadPokemon()  {
+    getAPIData(`https://pokeapi.co/api/v2/pokemon/lugia`).then(
+        (data => console.log(data))
+    )
+}
+
+loadPokemon()
