a8aa64781f0676e54ccceec7e651c6209e207c39
Kaden Johnson <nokadensly@gmail.com>
2022-04-10 18:36:48 -0600

    basic card structure

--- Portfolio/pokemon/pokecode.js
+++ Portfolio/pokemon/pokecode.js
@@ -1,15 +1,53 @@
-function getAPIData(url) {
+const getAPIData = async (url) => {
     try {
-        return fetch(url).then((data) => data.json())
+      const result = await fetch(url)
+      return await result.json()
     } catch (error) {
-        console.error(error)
+      console.error(error)
     }
+  }
+
+
+async function loadPokemon()  {
+   const pokeData = await getAPIData(`https://pokeapi.co/api/v2/pokemon/lugia`)
+   populatePokeGrid(pokeData)
 }
 
-function loadPokemon()  {
-    getAPIData(`https://pokeapi.co/api/v2/pokemon/lugia`).then(
-        (data => console.log(data))
-    )
+const pokeGird = document.querySelector('.pokeGrid')
+
+function populatePokeGrid(pokemonArray) {
+    populatePokeCard(pokemonArray [0])
 }
+
+function populatePokeCard(pokemon) {
+    const pokeScene = document.createElement('div')
+    pokeScene.className = 'scene'
+    const pokeCard = document.createElement('div')
+    pokeCard.className = 'card'
+    pokeCard.addEventListener('click', () => pokeCard.classList.toggle('is-flipped'))
+
+    pokeCard.appendChild(populateCardFront(pokemon))
+    pokeScene.appendChild(pokeCard)
+    pokeGird.appendChild(pokeScene)
+}
+
+function populateCardFront(pokemon) {
+   const pokeFront = document.createElement('figure')
+   pokeFront.className = 'cardFace'
+   const pokeImg = document.createElement('img')
+   pokeImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/143.png`
+   const pokeCaption = document.createElement('figcaption') 
+   pokeCaption.innerHTML = 'snorlax'
+
+   pokeFront.appendChild(pokeImg)
+   pokeFront.appendChild(pokeCaption)
+   return pokeFront
+}
+
+
+function populateCardBack(pokemon) {
+
+}
+
 
 loadPokemon()
