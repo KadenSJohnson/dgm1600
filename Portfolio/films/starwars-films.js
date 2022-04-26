32734b8d8b0246047311339d5b773b626be040b0
Kaden Johnson <nokadensly@gmail.com>
2022-04-25 20:43:10 -0600

    changed formating on poke id and started adding info to films page

--- Portfolio/films/starwars-films.js
+++ Portfolio/films/starwars-films.js
@@ -7,9 +7,23 @@ let filmList = document.querySelector('#filmlist')
 
 for (let i = 0; i < films.length; i++) {
 
+function movieinfo() {
+    return films.map(film => {
+        return {
+            director: film.director,
+            producer: film.producer,
+            release_date: film.release_date
+        }
+    })
+}
+
 
 let figure = document.createElement("figure")
+figure.addEventListener("click", () =>
+    figure.classList.toggle("popped") //add a way to annimate other information
+    );
 let figImage = document.createElement("img")
+let additionalInfo = document.createElement('p')
 let figCaption = document.createElement("figcaption")
 
 let filmNum = getLastNumber(films[i].url)
@@ -18,8 +32,10 @@ figImage.src = `https://starwars-visualguide.com/assets/img/films/${filmNum}.jpg
 
 figCaption.textContent = films[i].title
 
+additionalInfo.innerHTML = 
+
 figure.appendChild(figImage)
 figure.appendChild(figCaption)
-
+figure.appendChild(additionalInfo)
 filmList.appendChild(figure)
 }
